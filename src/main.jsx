74982c12dcf1e5ce4e02f3f47ae9b6e940ed205a
Lucas Vaz <httpslucasvaz@gmail.com>
2023-04-12 16:14:47 -0300

    feat: add GlobalStyle

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+import theme from './style/theme'
+import GlobalStyle from './style/global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
+    <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>,
 )
