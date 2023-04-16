00052c0fcafd1f5bda8ccfa7a237c9585cc93606
Lucas Vaz <httpslucasvaz@gmail.com>
2023-04-15 23:43:44 -0300

    update

---
+++ src/components/Home/styled.jsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  max-width: 1180px;
+  margin: auto;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 5rem);
+  align-items: center;
+  justify-content: center;
+
+  p:first-child {
+    font-size: clamp(2rem, 5vw, 4rem);
+    font-family: "Annie Use Your Telescope", "sans-serif";
+    color: ${({ theme }) => theme.colors.dark};
+    line-height: 0rem;
+    position: absolute;
+    margin-right: clamp(5rem, 20vw, 250px);
+  }
+
+  p {
+    font-size: clamp(6rem, 12vw, 12rem);
+    line-height: clamp(4rem, 8.9vw, 8.1rem);
+    font-family: "Kanit", "sans-serif";
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+export const Text = styled.span`
+  display: flex;
+  font-size: clamp(1.8rem, 4vw, 4rem);
+  font-family: "Annie Use Your Telescope", "sans-serif";
+  line-height: clamp(1.5rem, 3vw, 3rem);
+  justify-content: flex-end;
+`;
+
+export const TextContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: clamp(-2rem, -6rem, -9rem);
+    align-items: flex-end;
+  
+    img {
+    width: clamp(6rem, 12vw, 19rem);
+  }
+
+  img:last-child {
+    width: clamp(8rem, 14vw, 22rem);
+    margin-top: clamp(-0.8rem, -5rem, -6rem);
+  }
+`;
---
+++ src/components/Nav/styled.jsx
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+import { MdMenu, MdClose } from "react-icons/md";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 5rem;
+  background-color: ${(props) => props.theme.colors.primary};
+`;
+
+export const NavBar = styled.nav`
+  display: flex;
+  height: 100%;
+  max-width: 1180px;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+
+`;
+
+export const NavList = styled.div`
+  display: flex;
+  gap: 1em;
+  ul {
+    li {
+      display: flex;
+      list-style: none;
+      font-size: clamp(1em, 1.3vw, 1.2em);
+      font-weight: 600;
+      cursor: pointer;
+      padding: 0.5em 1em;
+      color: ${(props) => props.theme.colors.secondary};
+    }
+  }
+
+  @media (max-width: 900px) {
+    display: none;
+  }
+`;
+
+export const NavBarMobile = styled.nav`
+  height: calc(100vh - 5rem);
+  width: 100%;
+  position: absolute;
+  z-index: 999;
+  background-color: ${(props) => props.theme.colors.primary};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  ul {
+    justify-content: center;
+    align-items: center;
+
+    li {
+      display: flex;
+      list-style: none;
+      font-size: clamp(2em, 1.9vw, 3em);
+      font-weight: 600;
+      cursor: pointer;
+      padding: 0.5em 1em;
+      color: ${(props) => props.theme.colors.secondary};
+    }
+  }
+`;
+
+export const IconMdMenu = styled(MdMenu)`
+  display: none;
+  color: ${(props) => props.theme.colors.secondary};
+  font-size: 3em;
+  margin-right: 0.3em;
+
+  @media (max-width: 900px) {
+    display: block;
+  }
+`;
+
+export const IconMdClose = styled(MdClose)`
+  color: ${(props) => props.theme.colors.secondary};
+  font-size: 3em;
+  margin-right: 0.3em;
+
+  @media (max-width: 900px) {
+    display: block;
+  }
+`;
