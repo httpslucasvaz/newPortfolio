8064dc0be7b569d5b6cff55d2a88da38b22dd31f
Lucas Vaz <httpslucasvaz@gmail.com>
2023-04-16 23:56:30 -0300

    feat: add componente MyProjects

---
+++ src/components/MyProjects/index.jsx
@@ -0,0 +1,19 @@
+import { Title, Wrapper, Content } from "./styled";
+
+export function MyProjects() {
+  return (
+    <Wrapper>
+      <Content>
+        <Title>
+          <span>
+            <p>MEUS</p>
+            <p>
+              PRO <br />
+              JETOS
+            </p>
+          </span>
+        </Title>
+      </Content>
+    </Wrapper>
+  );
+}
---
+++ src/components/MyProjects/styled.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  max-width: 1180px;
+  margin: auto;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  padding: 1rem;
+
+  p:first-child {
+    font-size: clamp(2rem, 5vw, 4rem);
+    font-family: "Annie Use Your Telescope", "sans-serif";
+    color: ${({ theme }) => theme.colors.dark};
+    line-height: 0rem;
+    position: absolute;
+    margin-right: clamp(5rem, 20vw, 250px);
+  }
+
+  p {
+    display: block;
+    font-size: clamp(6rem, 12vw, 12rem);
+    line-height: clamp(4rem, 8.9vw, 8.1rem);
+    font-family: "Kanit", "sans-serif"; 
+    color: ${({ theme }) => theme.colors.primary};
+  }
+
+  span {
+    width: 100%;
+    height: auto;
+    border-right: 6px solid ${({ theme }) => theme.colors.primary};
+  }
+`;
