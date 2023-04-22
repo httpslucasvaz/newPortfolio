8fe32fb84b5bc60b3851b35f1acde956b8d1924f
Lucas Vaz <httpslucasvaz@gmail.com>
2023-04-22 03:10:14 -0300

    feat: add informações dos projetos

---
+++ src/data/myProjects.jsx
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: 1,
+        title: 'LOGIN.',
+        description: 'SHAUSHA SUAHSUASH SAHU',
+        url: 'https://github.com/httpslucasvaz/login-typescript-firebase',
+    },
+
+    {
+        id: 2,
+        title: 'POKEDEX.',
+        description: 'SHAUSHA SUAHSUASH SAHU',
+        url: 'https://github.com/httpslucasvaz/pokedex-react',
+    },
+
+    {
+        id: 3,
+        title: 'TODO LIST.',
+        description: 'SHAUSHA SUAHSUASH SAHU',
+        url: 'https://github.com/httpslucasvaz/todo-list-typescript-nextjs',
+    },
+]
+
+export default data
