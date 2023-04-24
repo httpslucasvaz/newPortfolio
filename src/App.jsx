6f4d8f1db03862a760a1d6eb2e6207dd97c132f2
Lucas Vaz <httpslucasvaz@gmail.com>
2023-04-23 21:31:43 -0300

    feat: add componente Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { AboutMe } from "./components/AboutMe";
+import { Contact } from "./components/Contact";
 import { Home } from "./components/Home";
 import { MyProjects } from "./components/MyProjects";
 import { Nav } from "./components/Nav";
@@ -10,6 +11,7 @@ function App() {
       <Home />
       <AboutMe />
       <MyProjects />
+      <Contact />
     </div>
   );
 }
