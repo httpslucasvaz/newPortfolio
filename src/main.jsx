51b62cd4312a54f04eec53deba449a710820ef40
Lucas Vaz <httpslucasvaz@gmail.com>
2023-04-27 00:11:33 -0300

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { ThemeProvider } from 'styled-components'
-import theme from './style/theme'
-import GlobalStyle from './style/global'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import theme from "./style/theme";
+import GlobalStyle from "./style/global";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <ThemeProvider theme={theme}>
-    <App />
-    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
